4df4d8ab1d568268d6d0473c114790e1d4bf107a
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-08-20 11:49:39 +0300

    add base icons in assets & registry icons as mat-icon

--- src/app/services/svg/svg.service.ts
+++ src/app/services/svg/svg.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SvgService {
 
-  constructor() { }
+  public readonly svgNamesList = [
+    'attach',
+    'bell',
+    'calendar',
+    'chevron-down',
+    'file',
+    'file-text',
+    'film',
+    'grid',
+    'img',
+    'message',
+    'more',
+    'send',
+    'person',
+    'plus',
+    'search',
+    'settings',
+    'smile',
+    'power'
+  ];
+
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.initSvg();
+  }
+
+  initSvg(): void {
+    this.svgNamesList.forEach((svgName: string) => {
+      this.matIconRegistry.addSvgIcon(
+        svgName,
+        this.domSanitizer.bypassSecurityTrustResourceUrl(`/assets/img/icons/${ svgName }.svg`)
+      );
+    });
+  }
 }
