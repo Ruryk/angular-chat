520794c07017752575319667f5e337bba3b226ab
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-08-25 18:21:31 +0300

    refactor main-routing.module routes array

--- src/app/template/main/main-routing.module.ts
+++ src/app/template/main/main-routing.module.ts
@@ -4,11 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './component/main.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent}
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'chat',
+      },
+      {
+        path: 'chat',
+        loadChildren: () =>
+          import('src/app/template/chat/chat.module').then((m) => m.ChatModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {}
