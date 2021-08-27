ed1342bd143f6c77dc3a5d600256c009168b57fd
Vlad Ivashchenko <mr.ruryks@gmail.com>
2021-08-27 11:45:01 +0300

    init delete chat, delete message, edit message modals

--- src/app/template/chat/children/chat-room/chat-room.module.ts
+++ src/app/template/chat/children/chat-room/chat-room.module.ts
@@ -6,9 +6,12 @@ import { ChatRoomRoutingModule } from './chat-room-routing.module';
 import { SharedModule } from '../../../../shared/shared.module';
 import { ReactiveComponentModule } from '@ngrx/component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DeleteMessageModalComponent } from './children/delete-message-modal/delete-message-modal.component';
+import { EditMessageModalComponent } from './children/edit-message-modal/edit-message-modal.component';
+import { DeleteChatModalComponent } from './children/delete-chat-modal/delete-chat-modal.component';
 
 @NgModule({
-  declarations: [ChatRoomComponent],
+  declarations: [ChatRoomComponent, DeleteMessageModalComponent, EditMessageModalComponent, DeleteChatModalComponent],
   imports: [
     CommonModule,
     ChatRoomRoutingModule,
